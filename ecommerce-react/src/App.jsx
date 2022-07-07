01e962374d8bbb7f2051935599ba95c622926666
Martin <martindao2082@gmail.com>
2022-07-07 00:11:33 -0700

    installed react router dom

--- ecommerce-react/src/App.jsx
+++ ecommerce-react/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import Header from './Header'
 import Home from './Home'
 import Books from './Books.json'
+import {BrowserRouter, Route, Router, Link} from 'react-router-dom';
 
 function App() {
   const {books} = Books;
